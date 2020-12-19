619076490074ea5bdd944680ace284b05fe408b6
OUNNOUGHENE mohand yacine <myacine.ounnoughene@gmail.com>
2020-12-19 18:44:53 +0100

    Ajout UML_CARD et Création de Card.hpp & Card.cpp

---
+++ src/Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mdyac on 19/12/2020.
+//
+
+#include "Card.hpp"
---
+++ src/Card.hpp
@@ -0,0 +1,14 @@
+//
+// Created by mdyac on 19/12/2020.
+//
+
+#ifndef CARD_GAMES_FRAMEWORK_CARD_HPP
+#define CARD_GAMES_FRAMEWORK_CARD_HPP
+
+
+class Card {
+
+};
+
+
+#endif //CARD_GAMES_FRAMEWORK_CARD_HPP
