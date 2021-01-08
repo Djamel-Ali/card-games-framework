b7c5ddab1de7cae428d62de566b54c64c8024501
Djamel-Ali <alidjamel365@gmail.com>
2021-01-08 13:06:00 +0100

    update player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
 #include "Player.hpp"
 
+// Initialization of static var
+int Player::player_id_counter = 0;
+
 // Ctor
-Player::Player(const int _uid, const string &_name, const vector<Card*> &_hand, int _score = 0) : unique_id(
+Player::Player(const int _uid, const string &_name, const vector<Card *> &_hand, int _score) : unique_id(
         _uid), name(_name), hand(_hand), current_score(_score) {}
+
+// copy ctor
+Player::Player(const Player &player) = default;
 
 //Dtor
 Player::~Player() = default;
@@ -17,7 +24,7 @@ const string &Player::getName() const {
     return name;
 }
 
-const vector<Card*> &Player::getHand() const {
+vector<Card *> &Player::getHand() {
     return hand;
 }
 
@@ -25,10 +32,22 @@ int Player::getCurrentScore() const {
     return current_score;
 }
 
+int Player::getFinalScore() const {
+    return final_score;
+}
+
 // Setters :
 
-void Player::setCurrentScore(int score) {
-    current_score = score;
+void Player::setCurrentScore(float _score) {
+    current_score = _score;
+}
+
+void Player::setFinalScore(int finalScore) {
+    final_score = finalScore;
+}
+
+void Player::setHand(const vector<Card *> &_hand) {
+    Player::hand = _hand;
 }
 
 // Other methods :
@@ -37,14 +56,75 @@ bool Player::handEmpty() {
     return hand.empty();
 }
 
+//todo : Player::playCard(arg) : il faut se mettre d'accord si on passe l'index ou l'ID en arg
 Card *Player::playCard(int index) {
-    Card* temp = hand.at(index);
+    Card *temp = hand.at(index);
 
     hand.erase(hand.begin() + index);
 
     return temp;
 }
 
+// ajoute la carte (au fait c'est son pointeur) passée en argument à la main du joueur courant
 void Player::addCard(Card *toAdd) {
     hand.push_back(toAdd);
+}
+
+int Player::get_fresh_player_uid() {
+    return player_id_counter++;
+}
+
+void Player::displayHand() const {
+    cout << "HAND : ";
+    auto counter = -1;
+    for (Card *pCard: hand) {
+        cout << "(" << counter++ << "): " << *pCard << " ; ";
+    }
+}
+
+// Remettre les cartes de la main du joueur courant dans le Deck passé en arg
+void Player::putBackHand(Deck &_deck) {
+    if(!handEmpty())
+    {
+        _deck.getDeckOfCards().insert(_deck.getDeckOfCards().end(), hand.begin(), hand.end());
+        hand.clear();
+    }
+}
+
+// Remettre les cartes gagnées dans la pioche (passée en arg)
+void Player::putBackReserve(Deck &_deck) {
+    if(!reserve.empty())
+    {
+        _deck.getDeckOfCards().insert(_deck.getDeckOfCards().end(), reserve.begin(), reserve.end());
+        reserve.clear();
+    }
+}
+
+vector<Card *> Player::getReserve() const {
+    return reserve;
+}
+
+void Player::setReserve(const vector<Card *> &_reserve) {
+    Player::reserve = _reserve;
+}
+
+int Player::getIndexOfCardToPlay() {
+    int chosen_index = -1;
+
+    //Afficher la main du joueur pour qu'il puisse choisir la carte à jouer
+    displayHand();
+
+    do {
+        cout << "Saisissez l'indice de la carte à jouer svp : ";
+        cin >> chosen_index;
+    }while (chosen_index < 0 or chosen_index > hand.size()-1);
+
+    return chosen_index;
+}
+
+
+std::ostream &operator<<(std::ostream &out, const Player &a_player) {
+    out << "\n\n\t" << a_player.getName() << " (id = " << a_player.getUniqueId() << ") "
+        << " ======> Score : " << a_player.getFinalScore() << endl;
+    return out;
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -4,8 +4,9 @@
 
 #include <string>
 #include <vector>
-
 #include "Card.hpp"
+#include "Deck.hpp"
+
 
 using namespace std;
 
@@ -14,12 +15,28 @@ class Player {
 private:
     const int unique_id;
     string name;
-    vector<Card*> hand;
+    vector<Card *> hand;
+    vector<Card *> reserve;
+
+    // score intermediaire (calculé pour chaque donne)
     int current_score;
+
+    // score final calculé à partir des scores intermediaires de chaque donne
+    int final_score;
+
+    // Used by the get_fresh_player_uid() method to generate unique IDs
+    static int player_id_counter;
+
+    // Generates a new identifier for each call that is different from the previous ones returned.
+    static int get_fresh_player_uid();
+
 public:
 
     // Ctor
-    Player(int _uid, const string &_name, const vector<Card*> &_hand, int _score);
+    Player(int _uid, const string &_name, const vector<Card *> &_hand, int _score = 0);
+
+    // copy ctor
+    Player(const Player &player);
 
     // Dtor
     virtual ~Player();
@@ -30,22 +47,48 @@ public:
 
     const string &getName() const;
 
-    const vector<Card*> &getHand() const;
+    vector<Card *> &getHand();
 
     int getCurrentScore() const;
 
+    int getFinalScore() const;
+
+    vector<Card *> getReserve() const;
+
+    int getIndexOfCardToPlay();
     // Setters :
 
-    void setCurrentScore(int score);
+    void setCurrentScore(float _score);
+
+    void setHand(const vector<Card *> &_hand);
+
+    void setReserve(const vector<Card *> &_reserve);
 
     // Other methods :
 
     bool handEmpty();
 
-    Card* playCard(int index);
+    //todo : Player::playCard(arg) : il faut se mettre d'accord si on passe l'index ou l'ID en arg
+    Card *playCard(int index);
 
-    void addCard(Card* toAdd);
+    // ajoute la carte (au fait c'est son pointeur) passée en argument à la main du joueur courant
+    void addCard(Card *toAdd);
+
+    void displayHand() const;
+
+    // Remettre les cartes de la main du joueur courant dans le Deck passé en arg
+    // (ça sert à remélanger et redistribuer par e.g)
+    void putBackHand(Deck &_deck);
+
+    // Remettre les cartes qui sont dans la réserve (cartes gagnées) du joueur courant dans le Deck passé en arg
+    // (ça sert à remélanger et redistribuer par e.g)
+    void putBackReserve(Deck &_deck);
+
+    void setFinalScore(int finalScore);
+
 };
 
+/// Surcharge de l’opérateur <<
+std::ostream &operator<<(std::ostream &out, const Player &a_game);
 
 #endif //CARD_GAMES_FRAMEWORK_PLAYER_HPP
