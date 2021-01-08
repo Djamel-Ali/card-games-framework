d78b2cf8a16dcf02e55feba4e99d9fa98da33630
Djamel-Ali <alidjamel365@gmail.com>
2021-01-08 13:00:46 +0100

    add first version of View Class

--- src/Game.hpp
+++ src/Game.hpp
@@ -15,10 +15,13 @@ private:
 protected:
     Deck deck;
     vector <Player*> joueurs;
+    const string name;
 
 public:
     explicit Game(const Deck &_deck);
     void initGame(); // mise en commun
+    static Card *getCardById(const vector<Card*> &vect_of_cards, int card_id);
+    static void moveFromTo(vector<Card*> src, vector<Card*> dest);
 
     virtual void createCards() = 0;
     virtual void startGame() = 0; // a mettre en commun plus tard
@@ -26,6 +29,8 @@ public:
     virtual int getWinner() = 0; // a mettre en commun plus tard (pour bataille, uno et 8us)
     virtual int getIndexOfParseCard() = 0;
     virtual void playRound(int indexCardToPlay) = 0;
+    virtual string getName() const = 0;
+    virtual Player *getPlayerByID(int _id) = 0;
 
 };
 
